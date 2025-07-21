968f6c156eb62a9c421a57525ddac0168aacd71c
anshit <anshit.kumar@tothenew.com>
2025-07-21 17:49:29 +0530

    setting up project

---
+++ src/main/java/com/ttn_spring/restfulwebserives/RestfulwebserivesApplication.java
@@ -0,0 +1,13 @@
+package com.ttn_spring.restfulwebserives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulwebserivesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulwebserivesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttn_spring/restfulwebserives/RestfulwebserivesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttn_spring.restfulwebserives;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulwebserivesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
